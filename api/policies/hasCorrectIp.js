c7a43799d29afb7cde0e85a1863254ac1f8394d6
fb@enyosolutions.com <fb@enyosolutions.com>
2021-03-19 16:30:45 +0000

    feat: loging real ips

--- api/policies/hasCorrectIp.js
+++ api/policies/hasCorrectIp.js
@@ -9,7 +9,7 @@ module.exports = function hasCorrectIp(req, res, next) {
   if (sails.config.security.disabledIpFiltering === true || sails.config.security.disabledIpFiltering === 1) {
     return next();
   }
-  console.log('requested ', req.path, 'with ip', req.ip, sails.config.security.whitelistedIps);
+  console.log('requested ', req.path, 'with ip', req.ip, 'Real ip', req.headers['x-real-ip'], sails.config.security.whitelistedIps);
   if (req.ip && sails.config.security && sails.config.security.whitelistedIps && (
 
     sails.config.security.whitelistedIps.includes(req.headers['x-real-ip'])
